aab0b5cce000cd74aaf8f2c117d61c110532126b
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-21 14:14:28 -0300

    feat(header.component.ts): adiciona importação de NavBarComponent, LogoComponent e CommonModule

--- corneteiro/src/app/components/header/header.component.ts
+++ corneteiro/src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [LogoComponent],
+  imports:
+  [
+    NavbarComponent,
+    LogoComponent,
+    CommonModule
+
+  ],
   templateUrl: './header.component.html',
+  standalone: true,
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+}
