c98f41b1e3830b99ecf7e6032388476779884611
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-23 16:05:18 -0300

    feat(app.routes.ts): cria rota para atletas

--- corneteiro/src/app/app.routes.ts
+++ corneteiro/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AtletasComponent } from './views/atletas/atletas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'atletas', component: AtletasComponent}
+];
