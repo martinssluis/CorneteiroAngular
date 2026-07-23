0a334d755e74777b0110c963ef8fdc7ee3e7463e
Luis Martins <luishenrique.martinslima@gmail.com>
2026-07-23 16:04:54 -0300

    feat(app.component.ts): adiciona standalone true e routeroutlet

--- corneteiro/src/app/app.component.ts
+++ corneteiro/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
-
+import { RouterOutlet } from "@angular/router";
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
-     HeaderComponent],
+    HeaderComponent,
+    RouterOutlet
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
